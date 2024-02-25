943e8d7702f84d5fa21a468e28e21018ef85ae7c
feliplourenco <felipecunegundes@outlook.com>
2024-02-24 23:35:16 -0300

    adicionando funcionalidades

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,19 @@
+export const dataFake = [
+    {
+      "id":"1",
+      "photo":"aaa",
+      "title": "Este é um titulo",
+      "description": "olá mundo!",
+      "photoCover": "https://sm.ign.com/ign_br/screenshot/default/attack-on-titan-featured-image-cropped_h3mq.jpg"
+    },
+
+    {
+      "id":"2",
+      "photo":"aaa",
+      "title": "Este é um titulo",
+      "description": "olá mundo!",
+      "photoCover": ""
+    }
+
+];
+
